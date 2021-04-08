069a9320a48c263a60d53e126e85f7a52c076f27
Aaron Smith <smitham50@gmail.com>
2021-04-08 10:39:45 -0400

    set up mongoose and make test campground in db

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', ()=> {
+    console.log('Database Connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,3 +27,14 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({
+        title: 'My Backyard',
+        price: '$20',
+        description: 'Could be worse.',
+        location: 'Brooklyn'
+    });
+    await camp.save();
+    res.send(camp);
+})
