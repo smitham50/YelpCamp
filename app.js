e89390bf0db8f349e0ff9049007a693e1fbf2c2a
Aaron Smith <smitham50@gmail.com>
2021-04-21 21:11:57 -0400

    add passport, passport-local and passport-local-mongoose and connect to user model

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const campgroundRouter = require('./routes/campgrounds');
 const reviewRouter = require('./routes/reviews');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -45,6 +47,13 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
