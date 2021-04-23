ca0e16ac784e52cf27fd9f479512dbff0a15b77d
Aaron Smith <smitham50@gmail.com>
2021-04-23 10:37:20 -0400

    add isAuthor middleware and apply to edit and delete routes and move validation middleware to middleware file

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,18 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { reviewSchema } = require('../schemas');
-const ExpressError = require('../utils/ExpressError');
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join('');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+const { validateReview } = require('../middleware');
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
