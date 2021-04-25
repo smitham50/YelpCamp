be519c3b5db21c4bea776c90ebc05dd5e4eaa8e1
Aaron Smith <smitham50@gmail.com>
2021-04-25 09:39:56 -0400

    add isReviewAuthor middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview, isLoggedIn, isAuthor } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
@@ -16,7 +16,7 @@ router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:reviewId', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
