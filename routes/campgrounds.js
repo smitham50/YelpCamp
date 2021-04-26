c4824e1825b13f22f61ceae864d1d9195e4c7d77
Aaron Smith <smitham50@gmail.com>
2021-04-25 21:11:08 -0400

    add user image upload to forms and image carousel to show--update campground model to hold multiple images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const { index, renderNewForm, createCampground, showCampground, renderEditForm, updateCampground, deleteCampground } = require('../controllers/campgrounds');
+const { 
+    index, 
+    renderNewForm, 
+    createCampground, 
+    showCampground, 
+    renderEditForm, 
+    updateCampground, 
+    deleteCampground 
+} = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary/index');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(index))
-    .post(isLoggedIn, validateCampground, catchAsync(createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(createCampground));
 
 router.get('/new', isLoggedIn, renderNewForm);
 
